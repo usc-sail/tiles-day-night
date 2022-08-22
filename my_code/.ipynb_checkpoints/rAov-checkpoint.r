bdcabe3ae3fe0c37ec41082ce9744eaef07ea02b
Detravious J. Brinkley <d.brinkley97@gmail.com>
2022-08-22 10:04:50 -0700

    cleaned table 2

--- my_code/.ipynb_checkpoints/rAov-checkpoint.r
+++ my_code/.ipynb_checkpoints/rAov-checkpoint.r
@@ -1,18 +1,18 @@
-rest_rest = function(at_work_df, off_work_df) {
+rest_model = function(at_work_df, off_work_df) {
     work_rest_model <- aov(rest ~ shift + age + gender, data = at_work_df)
     off_rest_model <- aov(rest ~ shift + age + gender, data = off_work_df)
     print(report(work_rest_model))
     print(report(off_rest_model))
 }
 
-step_d = function(at_work_df, off_work_df) {
+step_model = function(at_work_df, off_work_df) {
     work_rest_model <- aov(step_ratio ~ shift + age + gender, data = at_work_df)
     off_rest_model <- aov(step_ratio ~ shift + age + gender, data = off_work_df)
     print(report(work_rest_model))
     print(report(off_rest_model))
 }
 
-vig = function(at_work_df, off_work_df) {
+vigorous_model = function(at_work_df, off_work_df) {
     work_rest_model <- aov(vigorous_min ~ shift + age + gender, data = at_work_df)
     off_rest_model <- aov(vigorous_min ~ shift + age + gender, data = off_work_df)
     print(report(work_rest_model))
