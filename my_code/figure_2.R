bba203c8bbaea237772e1ff432d6b467147c1e1e
Detravious J. Brinkley <d.brinkley97@gmail.com>
2022-09-08 13:54:14 -0700

    add tables for off-days, need to optimize

--- my_code/figure_2.R
+++ my_code/figure_2.R
@@ -10,10 +10,11 @@ diurnal_rest_work = function(df_work) {
     df_work$id <- factor(df_work$id)
 #     print(df_work)
     rest_work_model <- ezANOVA(data=df_work, dv=rest, wid=.(id), within=.(time), between=.(shift), type=3)
-    print(rest_work_model)
+    
+    print(paste("********* START : Rest Work, Top Left *********"))
     posthoc = PostHocTest(aov(rest ~ shift*time, data=df_work), method = "lsd")
+    print(rest_work_model)
     print(posthoc)
-#     print(paste("********* END OF diurnal_rest_work *********"))
 }
 
 diurnal_rest_off = function(df_off) {
@@ -22,10 +23,12 @@ diurnal_rest_off = function(df_off) {
     df_off$id <- factor(df_off$id)
     
     rest_off_model <- ezANOVA(data=df_off, dv=rest, wid=.(id), within=.(time), between=.(shift), type=3)
-    print(rest_off_model)
+    
+    print(paste("********* START : Rest Off, Top Right *********"))
     posthoc = PostHocTest(aov(rest ~ shift*time, data=df_off), method = "lsd")
+    
+    print(rest_off_model)
     print(posthoc)
-#     print(paste("********* END OF diurnal_rest_off *********"))
 }
 
 diurnal_step_ratio_work = function(df_work) {
@@ -34,10 +37,12 @@ diurnal_step_ratio_work = function(df_work) {
     df_work$id <- factor(df_work$id)
 #     print(df_work)
     step_ratio_work_model <- ezANOVA(data=df_work, dv=step_ratio, wid=.(id), within=.(time), between=.(shift), type=3)
-    print(step_ratio_work_model)
+    
+    print(paste("********* START : Step/Walk Work, Bottom Left *********"))
     posthoc = PostHocTest(aov(step_ratio ~ shift*time, data=df_work), method = "lsd")
+    
+    print(step_ratio_work_model)
     print(posthoc)
-#     print(paste("********* END OF diurnal_rest_work *********"))
 }
 
 diurnal_step_ratio_off = function(df_off) {
@@ -46,10 +51,12 @@ diurnal_step_ratio_off = function(df_off) {
     df_off$id <- factor(df_off$id)
     
     step_ratio_off_model <- ezANOVA(data=df_off, dv=step_ratio, wid=.(id), within=.(time), between=.(shift), type=3)
-    print(step_ratio_off_model)
+    
+    print(paste("********* START : Step/Walk Off, Bottom Right *********"))
     posthoc = PostHocTest(aov(step_ratio ~ shift*time, data=df_off), method = "lsd")
+    
+    print(step_ratio_off_model)
     print(posthoc)
-#     print(paste("********* END OF diurnal_rest_off *********"))
 }
 
 
