072da5af1240e03030d67592b48d35469aafab6c
Detravious J. Brinkley <d.brinkley97@gmail.com>
2022-09-12 06:10:58 -0700

    code more optimized

--- my_code/figure2.R
+++ my_code/figure2.R
@@ -8,11 +8,12 @@ diurnal_rest_work = function(df_work) {
     df_work$time <- factor(df_work$time)
     df_work$shift <- factor(df_work$shift)
     df_work$id <- factor(df_work$id)
-#     print(df_work)
+    # print(df_work)
+    
     rest_work_model <- ezANOVA(data=df_work, dv=rest, wid=.(id), within=.(time), between=.(shift), type=3)
+    posthoc = PostHocTest(aov(rest ~ shift*time, data=df_work), method = "lsd")
     
     print(paste("********* START : Rest Work, Top Left *********"))
-    posthoc = PostHocTest(aov(rest ~ shift*time, data=df_work), method = "lsd")
     print(rest_work_model)
     print(posthoc)
 }
@@ -23,10 +24,9 @@ diurnal_rest_off = function(df_off) {
     df_off$id <- factor(df_off$id)
     
     rest_off_model <- ezANOVA(data=df_off, dv=rest, wid=.(id), within=.(time), between=.(shift), type=3)
-    
-    print(paste("********* START : Rest Off, Top Right *********"))
     posthoc = PostHocTest(aov(rest ~ shift*time, data=df_off), method = "lsd")
     
+    print(paste("********* START : Rest Off, Top Right *********"))
     print(rest_off_model)
     print(posthoc)
 }
@@ -36,11 +36,11 @@ diurnal_step_ratio_work = function(df_work) {
     df_work$shift <- factor(df_work$shift)
     df_work$id <- factor(df_work$id)
 #     print(df_work)
-    step_ratio_work_model <- ezANOVA(data=df_work, dv=step_ratio, wid=.(id), within=.(time), between=.(shift), type=3)
     
-    print(paste("********* START : Step/Walk Work, Bottom Left *********"))
+    step_ratio_work_model <- ezANOVA(data=df_work, dv=step_ratio, wid=.(id), within=.(time), between=.(shift), type=3)
     posthoc = PostHocTest(aov(step_ratio ~ shift*time, data=df_work), method = "lsd")
     
+    print(paste("********* START : Step/Walk Work, Bottom Left *********"))
     print(step_ratio_work_model)
     print(posthoc)
 }
